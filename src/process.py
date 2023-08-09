1814ca4368104b7002f733f1c276a71ac66f543d
copiumnicus <copiumnicus@gmail.com>
2023-08-09 11:43:00 +0100

    comments

--- src/process.py
+++ src/process.py
@@ -22,8 +22,11 @@ def protocol_overbid(solver, _df):
         "obj_value"].rank(ascending=False)
     proc["overbid"] = proc["score_norm"] > proc["obj_value"]
     proc = proc[proc["failed"] == False]
+    # get solvers where the obj value was best
     loss_auctions_best_val = proc[proc["rank"] == 1.0]
+    # but they did not win
     loss_auctions_best_val = loss_auctions_best_val[loss_auctions_best_val["ranking"] != 1]
+    # and auction submitted to chain
     loss_auctions_best_val = loss_auctions_best_val[loss_auctions_best_val["failed"] == False]
     # did not discount
     loss_auctions_best_val = loss_auctions_best_val[loss_auctions_best_val["score_norm"] >= loss_auctions_best_val["obj_value"]]
@@ -31,28 +34,36 @@ def protocol_overbid(solver, _df):
         "competitionSimulationBlock", inplace=True)
     print(loss_auctions_best_val)
 
+    # get overbidders that won
     overbidder = proc[proc["ranking"] == 1]
     overbidder = overbidder[overbidder["overbid"]]
     overbidder.set_index("competitionSimulationBlock", inplace=True)
+    # make sure that indexes match
     mask = loss_auctions_best_val.index.isin(overbidder.index)
     loss_auctions_best_val = loss_auctions_best_val[mask]
     overbidder = overbidder.loc[loss_auctions_best_val.index]
-    # loss_auctions = loss_auctions.loc[loss_auctions_rank2.index]
 
+    # obj loss is the loss of obj_val supplied by overbidder to the best solver
     overbidder["obj_loss"] = overbidder["obj_value"] - \
         loss_auctions_best_val["obj_value"]
+    # filter the small losses
     significant_idx = overbidder["obj_loss"] < -0.00005
     overbidder = overbidder[significant_idx]
     loss_auctions_best_val = loss_auctions_best_val[significant_idx]
+
+    # just obj loss in other direction, better label for scatter
     loss_auctions_best_val["solver_was_better_than_overbidder"] = loss_auctions_best_val["obj_value"] - \
         overbidder["obj_value"]
+    # labeling for scatter
     loss_auctions_best_val["overbidder"] = overbidder["solver"]
     loss_auctions_best_val["best"] = loss_auctions_best_val["solver"]
     loss_auctions_best_val["size"] = 0.25
     print(overbidder)
     print(loss_auctions_best_val)
+    # total obj loss of protocol in data
     print("TOTAL LOSS", overbidder["obj_loss"].sum())
 
+    # total loss of passed solver, obj value that the solver could have supplied
     solver_loss = loss_auctions_best_val[loss_auctions_best_val["solver"] == solver].index
     solver_loss = overbidder.loc[solver_loss]["obj_value"] - loss_auctions_best_val.loc[solver_loss]["obj_value"]
     print("TOTAL SOLVER LOSS", solver_loss.sum())
@@ -60,10 +71,6 @@ def protocol_overbid(solver, _df):
     fig = px.scatter_3d(loss_auctions_best_val,  x='best',
                         y="overbidder", z="solver_was_better_than_overbidder", color="solver",  size="size")
     fig.show()
-
-    # rank2 = proc[proc["rank"] == 2.0 and (proc["rank"] == 1 and proc["ranking"] != 1)]
-    # loss_auctions_raven = loss_auctions[loss_auctions["solver"] == "Raven"]
-    # print(loss_auctions_raven)
 
 
 # 4/ Surplus loss where obj value was smaller than overbid value
